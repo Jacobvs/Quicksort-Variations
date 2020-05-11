e8053c7d55ccbc81efdc28e65bcfdcefa55e30b2
Jacobvs <jacobvs2@gmail.com>
2020-05-11 12:52:42 -0700

    Push starter files

---
+++ src/DataGenerator.java
@@ -0,0 +1,15 @@
+public class DataGenerator {
+
+
+    public double[] generateNDoubles(int n){
+        return new double[n];
+    }
+
+    public String[] generateNStrings(int n){
+        return new String[n];
+    }
+
+    public Integer[] generateNIntegers(int n){
+        return new Integer[n];
+    }
+}
