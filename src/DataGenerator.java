24586c89148bac0e503106c5aa38dc7ecbc2e6a0
Jacobvs <jacobvs2@gmail.com>
2020-05-11 13:39:25 -0700

    Implement DataGenerator Methods

--- src/DataGenerator.java
+++ src/DataGenerator.java
@@ -1,15 +1,44 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class DataGenerator {
 
 
-    public double[] generateNDoubles(int n){
-        return new double[n];
+    public ArrayList<Double> generateNDoubles(int n){
+        ArrayList<Double> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            arr.add(StdRandom.uniform());
+
+        return arr;
     }
 
-    public String[] generateNStrings(int n){
-        return new String[n];
+    public ArrayList<String> generateNStrings(int n) throws FileNotFoundException {
+        ArrayList<String> strings = new ArrayList<>();
+        ArrayList<String> res = new ArrayList<>();
+        Scanner sc = new Scanner(new File("words.english.txt"));
+        while (sc.hasNext()) {
+            String s = sc.next();
+            strings.add(s);
+        }
+        sc.close();
+
+        for (int i = 0; i < n; i++) {
+            int index = StdRandom.uniform(0, strings.size());
+            res.add(strings.remove(index));
+        }
+        return res;
     }
 
-    public Integer[] generateNIntegers(int n){
-        return new Integer[n];
+    public ArrayList<Integer> generateNIntegers(int n){
+        ArrayList<Integer> ints = new ArrayList<>();
+
+        for (int i = 2; i < 17; i++)
+            for (int j = 0; j < n; j++)
+                ints.add(StdRandom.uniform(n/i));
+
+        return ints;
     }
+
 }
